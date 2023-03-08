1bf5e2249eabc23f9bde27cf75a8f94982e748de
NguyenNguyenNguyenPhong <109236721+NguyenNguyenNguyenPhong@users.noreply.github.com>
2023-03-08 21:57:10 +0700

    Final_NgocAnh_08_03

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Ngọc Anh';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 08-03';
+const titleCard = 'Tặng bạn Anh Mina';
+const contentCard = 'Chúc bạn ngày nào cũng tràn ngập hoa hồng nhé! Chúc các bạn thật tươi tắn, khỏe khoắn, luôn xinh xắn, đáng yêu.Sớm có 10 người yêu';
+
+
+const giftImage = 'NgocAnh.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
